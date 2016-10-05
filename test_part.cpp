4b40aaa8afa0327aecc14bcbdb2f949bf9da5eaa
Will <wldarden@gmail.com>
2016-10-05 12:40:45 -0500

    Add test File

---
+++ test_part.cpp
@@ -0,0 +1,18 @@
+//
+//  test_part.cpp
+//  
+//
+//  Created by Will Darden on 10/5/16.
+//
+//
+
+#include <stdio.h>
+#include <Part.hpp>
+
+int main(){
+    bool pass = TRUE;
+    
+    Part p("Test Name", 42, 1.0, 2.0, "Test Description");
+    
+    
+}
