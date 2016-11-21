eafe6adf8a6f157b6a8322d1577b159900a49a07
Will <wldarden@gmail.com>
2016-11-20 19:49:27 -0600

    Ability to create a part completed

--- MainGui2.cpp
+++ MainGui2.cpp
@@ -56,13 +56,15 @@ Shop shop;
 
 /////////////Create Part Widgets
 // group list
+list<Fl_Widget*> allWidgets;
 list<Fl_Widget*> cPartView;
 list<Fl_Widget*> cPartInputs;
 list<Fl_Widget*> activeInputs;
 
-//group zero
+//group drawing zero
 int x = 100;
 int y = 100;
+int currentPartType = 0;
 Fl_Input *nameInput = new Fl_Input(x,y,200,20,"Name");
 Fl_Input *pNumInput = new Fl_Input(x,y+30,200,20,"Part Number");
 Fl_Int_Input *weightInput = new Fl_Int_Input(x,y+60,200,20,"Weight");
@@ -75,9 +77,8 @@ Fl_Int_Input *batPowerInput = new Fl_Int_Input(x+350,y,200,20,"Max Power");
 //Locomotor
 Fl_Int_Input *maxSpeedInput = new Fl_Int_Input(x+350,y,200,20,"Max Speed");
 //Battery Compartments
-Fl_Int_Input *batCompartmentsInput = new Fl_Int_Input(x+350,y,200,20,"Battery Compartments (max 3)");
-// Fl_Input *costInput = new Fl_Input(x,y+90,200,20,"Cost");
-// Fl_Input *descInput = new Fl_Input(x,y+120,200,20,"Description");
+Fl_Int_Input *batCompartmentsInput = new Fl_Int_Input(x+350,y,200,20,"Bat. Slots (max 3)");
+
 //Create Customer Widgets
 
 ///////////////////////MISC METHODS
@@ -101,15 +102,78 @@ void addToActive(list<Fl_Widget*> toAdd){
   }
 }
 
+void addToAll(list<Fl_Widget*> toAdd){
+  for(Fl_Widget* w:toAdd){
+    allWidgets.push_back(w);
+  }
+}
+
+void setDefaults(){
+  nameInput->value("DefaultName");
+  pNumInput->value("1");
+  weightInput->value("1");
+  costInput->value("1");
+  descInput->value("DefaultDescription");
+  batPowerInput->value("1");
+  maxSpeedInput->value("1");
+  batCompartmentsInput->value("1");
+}
+
 ///////////////////////CREATE PART VIEW CallBacks
-void saveInfo_CB(Fl_Widget *w, void* p){
+void saveInfo_CB(Fl_Widget *wid, void* p){
+  //initialize input variables
+  string name, desc;
+  double weight, cost, max_power, bat_comp, max_speed;
+  int id, batComp;
+  PartType type;
 
+  //get input Variables
+  name = nameInput->value();
+  id = atoi(pNumInput->value());
+  weight = atof(weightInput->value());
+  cost = atof(costInput->value());
+  desc = descInput->value();
+  max_power = atof(batPowerInput->value());
+  max_speed = atof(maxSpeedInput->value());
+  bat_comp = atoi(batCompartmentsInput->value());
+  type = PartType(currentPartType);
 
+  switch(currentPartType){
+      case(1):{
+          //Arm
+          shop.add_part(new Arm(name,id,type,weight,cost,desc));
+          break;
+      }
+      case(2):{
+          //battery
+          shop.add_part(new Battery(name,id,type,weight,cost,desc,max_power));
+          break;
+      }
+      case(3):{
+          //head
+          shop.add_part(new Head(name,id,type,weight,cost,desc));
+          break;
+      }
+      case(4):{
+          //locomotor
+          shop.add_part(new Locomotor(name,id,type,weight,cost,desc,max_speed));
+          break;
+      }
+      case(5):{
+          //torso
+          shop.add_part(new Torso(name,id,type,weight,cost,desc,bat_comp));
+          break;
+      }
+  }
+  toggleView(activeInputs,0);
+  toggleView(cPartView,0);
 }
 
 ///////////////////////CREATE MENU CALLBACKS
 //Create Part CallBacks
 void createPart_CB(Fl_Widget *w, void* p){
+  //reset view
+  toggleView(allWidgets,0);
   //save part_type
   int part_type = *((int*) p);
   PartType type = PartType(part_type);
@@ -119,47 +183,36 @@ void createPart_CB(Fl_Widget *w, void* p){
   addToActive(cPartInputs);
   //for all parts, get this info
   toggleView(cPartView,ON);
-
+  setDefaults();
+  currentPartType = part_type;
   switch(part_type){
     case(1):{
         //Arm
         break;
     }
-    // case(2):{
-    //     //battery
-    //     double max_power;
-    //     cout << "Battery max power :";
-    //     cin >> max_power;
-    //     cin.ignore();
-    //     shop.add_part(new Battery(name,id,type,weight,cost,desc,max_power));
-    //     break;
-    // }
-    // case(3):{
-    //     //head
-    //     break;
-    // }
-    // case(4):{
-    //     //locomotor
-    //     int max_speed;
-    //     cout << "Locomotor max speed: ";
-    //     cin >> max_speed;
-    //     cin.ignore();
-    //     shop.add_part(new Locomotor(name,id,type,weight,cost,desc,max_speed));
-    //     break;
-    // }
-    // case(5):{
-    //     //torso
-    //     int bat_comp;
-    //     cout << "Number of Battery Compartments: ";
-    //     cin >> bat_comp;
-    //     cin.ignore();
-    //     shop.add_part(new Torso(name,id,type,weight,cost,desc,bat_comp));
-    //     break;
-    // }
-// }
+    case(2):{
+        //battery
+        batPowerInput->show();
+        activeInputs.push_back(batPowerInput);
+        break;
+    }
+    case(3):{
+        //head
+        break;
+    }
+    case(4):{
+        //locomotor
+        maxSpeedInput->show();
+        activeInputs.push_back(maxSpeedInput);
+        break;
+    }
+    case(5):{
+        //torso
+        batCompartmentsInput->show();
+        activeInputs.push_back(batCompartmentsInput);
+        break;
+    }
    }
-  // toggleView(cPartView, ON);
-
 }
 
 void createTorso_CB(Fl_Widget *w, void* p){
@@ -194,7 +247,21 @@ void createCustomer_CB(Fl_Widget *w, void* p){
 }
 
 int main(){
+  //create Main Screen
+  int narm=1,nbat=2,nhead=3,nloco=4,ntorso=5;
+  menu->add("File/Quit",   FL_CTRL+'q', Quit_CB);
+  menu->add("Create/Part/Arm","+a",createPart_CB, &narm);
+  menu->add("Create/Part/Battery","+b",createPart_CB, &nbat);
+  menu->add("Create/Part/Head","+h",createPart_CB,(void*) &nhead);
+  menu->add("Create/Part/Locomotor","+l",createPart_CB, &nloco);
+  menu->add("Create/Part/Torso","+t",createPart_CB, &ntorso);
+  menu->add("Create/Robot",FL_CTRL+'r', createRobot_CB);
+  menu->add("Create/Sales Associate",FL_CTRL+'s', createSA_CB);
+  menu->add("Create/Customer",FL_CTRL+'c', createCustomer_CB);
+  menu->add("Reports/Part");
+
   //////////Initiate part screen
+  //add parts to group list handles
   cPartView.push_back(nameInput);
   cPartView.push_back(pNumInput);
   cPartView.push_back(weightInput);
@@ -206,27 +273,19 @@ int main(){
   cPartInputs.push_back(weightInput);
   cPartInputs.push_back(costInput);
   cPartInputs.push_back(descInput);
+  saveButton->callback(saveInfo_CB);
+  //set view
   batPowerInput->hide();
   maxSpeedInput->hide();
   batCompartmentsInput->hide();
   toggleView(cPartView, 0);
 
-  //create menu bar
-  int x = 1;
-  menu->add("File/Quit",   FL_CTRL+'q', Quit_CB);
-  menu->add("Create/Part/Arm","+a",createPart_CB, &x);
-  x++;
-  menu->add("Create/Part/Battery","+b",createPart_CB, &x);
-  x++;
-  menu->add("Create/Part/Head",0,createPart_CB,(void*) &x);
-  x++;
-  menu->add("Create/Part/Locomotor","+l",createPart_CB, &x);
-  x++;
-  menu->add("Create/Part/Torso","+t",createPart_CB, &x);
-  menu->add("Create/Robot",FL_CTRL+'r', createRobot_CB);
-  menu->add("Create/Sales Associate",FL_CTRL+'s', createSA_CB);
-  menu->add("Create/Customer",FL_CTRL+'c', createCustomer_CB);
-  menu->add("Reports/Part");
+  /////////////////set global list
+  addToAll(cPartView);
+  allWidgets.push_back(batPowerInput);
+  allWidgets.push_back(maxSpeedInput);
+  allWidgets.push_back(batCompartmentsInput);
+
 
 
   win->end();
